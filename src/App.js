533afefa39769642d42b83ea285b860a25b09448
Lucas Tan <sheng.x.tan@gmail.com>
2022-06-24 23:17:10 -0600

    add images and styling in css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+const api = {
+  key: "59ab30a93ba5f064e22240a79c4a3edc",
+  base: "https://api.openweathermap.org/data/2.5"
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app warm">
+      <main>
+        <div className="search-box">
+          <input 
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            />
+        </div>
+      </main>
     </div>
   );
 }
