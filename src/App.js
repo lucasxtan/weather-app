a54d9e1ee5d90fbfccb32cb58fe29f9f485bd0ed
Lucas Tan <sheng.x.tan@gmail.com>
2022-06-26 23:45:54 -0600

    make location dynamic

--- src/App.js
+++ src/App.js
@@ -11,52 +11,58 @@ function App() {
   const [weather, setWeather] = useState({});
 
   const search = evt => {
-    if (evt.key === "Enter"){
+    if (evt.key === "Enter") {
       fetch(`${api.base}weather?q=${query}&units=metric&APID=${api.key}`)
-      .then(res => res.json())
-      .then(result => {
-        setWeather(result)});
-        setQuery('');
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+        });
+      setQuery('');
+      console.log(result);
     }
-  }
+  };
 
   const dateBuilder = (d) => {
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  
+
     let day = days[d.getDay()];
     let date = d.getDate();
     let month = months[d.getMonth()];
     let year = d.getFullYear();
 
-    return `${day} ${date} ${month} ${year}`
-  }
+    return `${day} ${date} ${month} ${year}`;
+  };
 
   return (
     <div className="app warm">
       <main>
         <div className="search-box">
-          <input 
+          <input
             type="text"
             className="search-bar"
             placeholder="Search..."
             onChange={(e => setQuery(e.target.value))}
             value={query}
             onKeyPress={search}
-            />
+          />
         </div>
-        <div className="location-box">
-          <div className="location">New York City, US</div>
-          <div className="date">{dateBuilder(new Date())}</div>
-        </div>
-        <div className="weather-box">
-          <div className="temp">
-            15°C
+        {(typeof weather.main != "undefined") ? (
+          <div>
+            <div className="location-box">
+              <div className="location">{weather.name}, {weather.sys.country}</div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">
+                15°C
+              </div>
+              <div className="weather">
+                Sunny
+              </div>
+            </div>
           </div>
-          <div className="weather">
-            Sunny
-          </div>
-        </div>
+        ) : ('')}
       </main>
     </div>
   );
